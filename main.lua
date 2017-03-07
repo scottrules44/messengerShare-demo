a3ca396a888a9a2cf26eddb4e3f60e91c6f8aa32
Scott Harrrison <scottrules44@gmail.com>
2017-03-06 18:47:33 -0600

    Bug fixes
    Bug fixes

--- main.lua
+++ main.lua
@@ -1,5 +1,80 @@
 local messengerShare = require "plugin.messengerShare"
 local widget = require("widget")
+-- for android file system
+local function doesFileExist( fname, path )
+
+    local results = false
+
+    -- Path for the file
+    local filePath = system.pathForFile( fname, path )
+
+    if ( filePath ) then
+        local file, errorString = io.open( filePath, "r" )
+
+        if not file then
+            -- Error occurred; output the cause
+            print( "File error: " .. errorString )
+        else
+            -- File exists!
+            print( "File found: " .. fname )
+            results = true
+            -- Close the file handle
+            file:close()
+        end
+    end
+
+    return results
+end
+function copyFile( srcName, srcPath, dstName, dstPath, overwrite )
+
+    local results = false
+
+    local fileExists = doesFileExist( srcName, srcPath )
+    if ( fileExists == false ) then
+        return nil  -- nil = Source file not found
+    end
+
+    -- Check to see if destination file already exists
+    if not ( overwrite ) then
+        if ( fileLib.doesFileExist( dstName, dstPath ) ) then
+            return 1  -- 1 = File already exists (don't overwrite)
+        end
+    end
+
+    -- Copy the source file to the destination file
+    local rFilePath = system.pathForFile( srcName, srcPath )
+    local wFilePath = system.pathForFile( dstName, dstPath )
+
+    local rfh = io.open( rFilePath, "rb" )
+    local wfh, errorString = io.open( wFilePath, "wb" )
+
+    if not ( wfh ) then
+        -- Error occurred; output the cause
+        print( "File error: " .. errorString )
+        return false
+    else
+        -- Read the file and write to the destination directory
+        local data = rfh:read( "*a" )
+        if not ( data ) then
+            print( "Read error!" )
+            return false
+        else
+            if not ( wfh:write( data ) ) then
+                print( "Write error!" )
+                return false
+            end
+        end
+    end
+
+    results = 2  -- 2 = File copied successfully!
+
+    -- Close file handles
+    rfh:close()
+    wfh:close()
+
+    return results
+end
+copyFile( "rickRoll.mp4.txt", nil, "rickRoll.mp4", system.DocumentsDirectory, true )
 
 local bg = display.newRect( display.contentCenterX, display.contentCenterY, display.actualContentWidth, display.actualContentHeight )
 local messangerIcon = display.newImage("messanger.png")
@@ -15,7 +90,7 @@ shareButton1 = widget.newButton( {
         label = "Share Url",
         onEvent = function ( e )
                 if (e.phase == "ended") then
-                       messengerShare.show({url="https://scotth.tech", quote = "insert cool quote here"}) 
+                       messengerShare.show({url="https://scotth.tech", quote = "insert cool quote here"})
                 end
         end
 } )
@@ -43,8 +118,8 @@ if (system.getInfo("platform")~= "ios") then
 	        label = "Share Video",
 	        onEvent = function ( e )
 	                if (e.phase == "ended") then
-	                       local coronaVideo = system.pathForFile( "rickRoll.mp4" )
-							messengerShare.show({video=coronaVideo})
+	                       local coronaVideo = system.pathForFile( "rickRoll.mp4", system.DocumentsDirectory )
+							           messengerShare.show({video=coronaVideo})
 	                end
 	        end
 	} )
